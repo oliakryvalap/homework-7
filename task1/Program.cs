8112550c91bfd962216c1f2232228464ec37b35c
Olga <olia_tarasenko@inbox.ru>
2023-03-03 19:40:24 +0300

    Задача 1

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+//1 -3,3 8 -9,9
+
+//8 7,8 -7,1 9
+int[,] array = new int[3, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    Console.WriteLine();
+   for (int j = 0; j < array.GetLength(0); j++) 
+   {
+   array[i, j] = new Random().Next(-10, 11);
+   Console.Write(array[i, j] + " ");
+}
+}
